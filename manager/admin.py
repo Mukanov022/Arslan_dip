96038aad4da2f14cebdc529b68fc6f670731018f
zloisupport <zlo1supp0rt@gmail.com>
2023-05-16 23:20:02 +0600

    Export to PDF

--- manager/admin.py
+++ manager/admin.py
@@ -2,8 +2,80 @@ import csv
 
 from django.contrib import admin
 from django.http import HttpResponse
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from io import BytesIO
+from django.conf import settings
 
+from device_manager.settings import BASE_DIR
 from .models import *
+
+
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from io import BytesIO
+from django.conf import settings
+
+
+def export_to_pdf(modeladmin, request, queryset):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="export.pdf"'
+
+    if settings.DEBUG:
+        font_path = "http://127.0.0.1:8000" + settings.STATIC_URL + "dejavuserif.ttf"
+    else:
+        font_path = BASE_DIR / "static/vendor/assets/fonts/dejavuserif.ttf"
+
+    pdfmetrics.registerFont(TTFont('DejaVuSans', font_path))
+
+    pdf_buffer = BytesIO()
+    doc = SimpleDocTemplate(pdf_buffer, pagesize=letter)
+    styles = getSampleStyleSheet()
+
+    fields = modeladmin.list_display
+    verbose_names = [modeladmin.model._meta.get_field(field).verbose_name for field in fields]
+
+    cyrillic_style = ParagraphStyle(
+        name='CyrillicStyle',
+        parent=styles['Normal'],
+        fontName='DejaVuSans',
+    )
+
+    data = [verbose_names]
+    for obj in queryset:
+        row = [
+            Paragraph(str(getattr(obj, field)), cyrillic_style) for field in fields
+        ]
+        data.append(row)
+
+    table = Table(data)
+    table_style = TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.gray),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'DejaVuSans'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ])
+    table.setStyle(table_style)
+
+    elements = [table]
+    doc.build(elements)
+
+    pdf = pdf_buffer.getvalue()
+    pdf_buffer.close()
+    response.write(pdf)
+
+    return response
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -30,6 +102,7 @@ def export_to_csv(modeladmin, request, queryset):
 
 
 export_to_csv.short_description = "Экспорт в CSV"
+export_to_pdf.short_description = "Экспорт в PDF"
 
 
 @admin.register(Device)
@@ -50,8 +123,7 @@ class DeviceTypeAdmin(admin.ModelAdmin):
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ["personal_number", "first_name", "last_name", "role"]
     search_fields = ("last_name__startswith",)
-    actions = [export_to_csv]
-    actions = [export_to_csv]
+    actions = [export_to_pdf,export_to_csv]
 
 
 @admin.register(Settings)
